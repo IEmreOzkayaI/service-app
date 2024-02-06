8299a98e7fc8f424ad1a4a0a877d9045a8a608db
Emre Ozkaya <emre.ozkaya@tokeninc.com>
2024-02-06 11:46:28 +0300

    Finish: final of the project

--- screens/Directions.js
+++ screens/Directions.js
@@ -1,13 +1,17 @@
-import React, {useEffect} from 'react';
-import {View, Text, Image, StyleSheet, ScrollView, Dimensions, TouchableOpacity, FlatList} from 'react-native';
-import BackgroundImage from "../assets/icon.png";
+import React, {useEffect, useState} from 'react';
+import {View, Text, Image, StyleSheet, ScrollView, Dimensions, TouchableOpacity, Button, Alert} from 'react-native';
 import Icon from 'react-native-vector-icons/AntDesign';
+import * as SQLite from 'expo-sqlite';
 
-function Directions({ route, navigation }) {
-    const { image, data } = route.params;
-    const [cardHeight, setCardHeight] = React.useState(400);
-    const [stretchCard, setStretchCard] = React.useState(false);
-    const [dataList, setDataList] = React.useState([]); // Initialize dataList state
+const db = SQLite.openDatabase('directions.db');
+
+const Directions = ({route, navigation}) => {
+    const {image, data} = route.params;
+    const [cardHeight, setCardHeight] = useState(400);
+    const [stretchCard, setStretchCard] = useState(false);
+    const [dataList, setDataList] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(0);
+
     const handleCardHeight = () => {
         if (cardHeight === 400) {
             const screenHeight = Dimensions.get('window').height - 150;
@@ -15,53 +19,74 @@ function Directions({ route, navigation }) {
         } else {
             setCardHeight(400);
         }
-    }
+    };
+
+    const handleApprove = () => {
+        // Eğer "Onayla" butonuna basılırsa direkt olarak Home ekranına yönlendir
+        Alert.alert("Arıza Başarı ile Giderilmiştir.")
+        navigation.navigate('Home');
+    };
+
+    const handleReject = () => {
+        if (currentIndex < dataList.length - 1) {
+            setCurrentIndex((prevIndex) => prevIndex + 1);
+        } else {
+            Alert.alert("Sorun çözülmedi ise ürün depoya gönderilmelidir.")
+            navigation.navigate('Home');
+        }
+    };
 
     useEffect(() => {
         if (data?.description) {
             const processedDataList = data.description.replace(/[\[\]]/g, '').split(',').map(item => item.replace(/['"]+/g, '').trim());
-            setDataList(processedDataList); // Update the dataList state
+            setDataList(processedDataList);
         }
     }, [data]);
 
     return (
         <ScrollView>
-            <View style={[styles.directions__page,]}>
-                <View style={[styles.directions__page__main__navbar]}>
-                    <Text style={[styles.directions__page__main__navbar__text]}>Hata Kodu: {data?.error_code}</Text>
-                    <Icon style={styles.directions__page__main__navbar__icon} name="close" size={20} color="#000" onPress={() => navigation.navigate('Home')} />
+            <View style={styles.directions__page}>
+                <View style={styles.directions__page__main__navbar}>
+                    <Text style={styles.directions__page__main__navbar__text}>Hata Kodu: {data?.error_code}</Text>
+                    <Icon style={styles.directions__page__main__navbar__icon} name="close" size={20} color="#000"
+                          onPress={() => navigation.navigate('Home')}/>
                 </View>
-                <View style={[styles.directions__page__navbar]}>
-                    <Text style={[styles.directions__page__header, stretchCard ? { display: "none" } : null]}>{data?.name}</Text>
+                <View style={styles.directions__page__navbar}>
+                    <Text
+                        style={[styles.directions__page__header, stretchCard ? {display: 'none'} : null]}>{data?.name}</Text>
                 </View>
-                <View style={[styles.directions__page__image, stretchCard ? { display: "none" } : null]}>
-                    {!image && <Image source={BackgroundImage} style={styles.directions__page__image__index} />}
-                    {image && <Image resizeMode={"contain"} source={{ uri: image }} style={styles.directions__page__image__index} />}
+                <View style={[styles.directions__page__image, stretchCard ? {display: 'none'} : null]}>
+                    {!image && <Image source={BackgroundImage} style={styles.directions__page__image__index}/>}
+                    {image && <Image resizeMode="contain" source={{uri: image}}
+                                     style={styles.directions__page__image__index}/>}
                 </View>
-                <View style={[styles.directions__page__content, { height: cardHeight } , stretchCard ? null:{ marginTop:35}]}>
+                <View
+                    style={[styles.directions__page__content, {height: cardHeight}, stretchCard ? null : {marginTop: 35}]}>
                     <View style={styles.directions__page_content__header}>
-                        <Text style={[styles.directions__page_content__header__text ]}>{data?.short_description}</Text>
+                        <Text style={styles.directions__page_content__header__text}>{data?.short_description}</Text>
                     </View>
-                    <ScrollView   style={[styles.directions__page__content_body]}>
-                        {dataList.map((item, index) => (
-                            <Text key={index} style={styles.directions__page__content_body__item}>{index+1}- {item}</Text>
-                        ))}
+                    <ScrollView style={styles.directions__page__content_body}>
+                        <Text style={styles.directions__page__content_body__item}>{dataList[currentIndex]}</Text>
                     </ScrollView>
-                    <TouchableOpacity style={styles.directions__page__content__button} onPress={() => {
-                        setStretchCard(!stretchCard);
-                        handleCardHeight()
-                    }}>
-                        {stretchCard ? (
-                            <Text style={styles.directions__page__content__button__text}>Detayları Gizle</Text>
-                        ) : (
-                            <Text style={styles.directions__page__content__button__text}>Detayları Göster</Text>
-                        )}
-                    </TouchableOpacity>
+                    <Text style={styles.directions__page__content__buttons}>
+                        <TouchableOpacity style={[styles.directions__page__content__button, styles.redButton]}
+                                          onPress={handleReject}>
+                            <Text
+                                style={[styles.directions__page__content__button__text, styles.redButton__text__color]}>Çözülmedi</Text>
+                        </TouchableOpacity>
+                        <View style={{width: 20}}></View>
+                        <TouchableOpacity style={[styles.directions__page__content__button, styles.greenButton]}
+                                          onPress={handleApprove}>
+                            <Text
+                                style={[styles.directions__page__content__button__text, styles.greenButton__text__color]}>Çözüldü</Text>
+                        </TouchableOpacity>
+                    </Text>
                 </View>
             </View>
         </ScrollView>
     );
-}
+};
+
 
 const styles = StyleSheet.create({
     directions__page: {
@@ -69,6 +94,27 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         overflow: 'scroll',
         paddingTop: 50,
+    },
+
+
+    directions__page__content__buttons: {},
+
+    redButton: {
+        backgroundColor: 'transparent',
+        borderColor: 'black',
+    },
+
+    greenButton: {
+        backgroundColor: 'black',
+        borderColor: 'white',
+    },
+
+    redButton__text__color: {
+        color: 'black',
+    },
+
+    greenButton__text__color: {
+        color: 'white',
     },
 
     directions__page__main__navbar: {
@@ -153,7 +199,7 @@ const styles = StyleSheet.create({
     },
     directions__page__content_body: {
         width: 300,
-        textAlign:'justify'
+        textAlign: 'justify'
     },
     directions__page__content_body__item: {
         fontSize: 15,
@@ -161,23 +207,22 @@ const styles = StyleSheet.create({
 
     },
     directions__page__content__button: {
-        width: 300,
-        height: 40,
+        width: 150,
+        height: 15,
         borderColor: '#27374D',
         borderWidth: 1, // Kenarlık kalınlığı
-        borderRadius: 10, // Kenarlık yardarm
+        borderRadius: 5, // Kenarlık yardarm
+        userSelect: 'none',
         alignItems: 'center',
         justifyContent: 'center',
-        userSelect: 'none',
         zIndex: 999,
         marginBottom: 20,
     },
     directions__page__content__button__text: {
-        fontSize: 20,
+        fontSize: 14,
         fontWeight: 'bold',
         textAlign: 'center',
         lineHeight: 40,
-        color: '#27374D',
     }
 });
 
